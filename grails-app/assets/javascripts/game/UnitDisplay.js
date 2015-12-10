661377d1c7e541563c3249db33859bcdc01af6f3
Eric H <harbdog@gmail.com>
2015-12-09 19:03:51 -0500

    Changed enemy unit symbol indicator from a 4 pointed star to triangle

--- grails-app/assets/javascripts/game/UnitDisplay.js
+++ grails-app/assets/javascripts/game/UnitDisplay.js
@@ -585,7 +585,7 @@ c.updateUnitIndicator = function() {
 		}
 		else{
 			color = Settings.get(Settings.UI_ENEMY_COLOR);
-			this.indicator.graphics.setStrokeStyle(3, "round").beginStroke(color).drawPolyStar(0, 0, hexWidth/3, 4, 0.5, -90).endStroke();
+			this.indicator.graphics.setStrokeStyle(3, "round").beginStroke(color).drawPolyStar(0, 0, hexWidth/3, 3, 0.5, -90).endStroke();
 		}
 		
 		// give the indicator a glow
@@ -612,7 +612,7 @@ c.showTurnDisplay = function(show) {
 	}
 	else {
 		color = Settings.get(Settings.UI_ENEMY_COLOR);
-		this.indicator.graphics.setStrokeStyle(2, "round").beginStroke(color).drawPolyStar(0, 0, hexWidth/8, 4, 0.5, -90).endStroke();
+		this.indicator.graphics.setStrokeStyle(2, "round").beginStroke(color).drawPolyStar(0, 0, hexWidth/8, 3, 0.5, -90).endStroke();
 	}
 	
 	// give the indicator a glow
